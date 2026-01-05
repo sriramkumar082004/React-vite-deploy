f60482576571c6380c13b8cdd7d6fc3ea1223061
Sriram Kumar <sriramkumar082004@gmail.com>
2026-01-05 19:28:50 +0530

    Api Configuration

--- src/api.js
+++ src/api.js
@@ -4,8 +4,11 @@ export const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
 });
 
+
 export const getStudents = () => API.get("/students");
 export const addStudent = (student) => API.post("/students", student);
 export const deleteStudent = (id) => API.delete(`/students/${id}`);
 export const updateStudent = (id, student) =>
   API.put(`/students/${id}`, student);
+
+
