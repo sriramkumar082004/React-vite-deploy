657958d7709985951f1a097c1d5ed1a1c6eeccb2
Sriram Kumar <sriramkumar082004@gmail.com>
2026-01-06 10:17:25 +0530

    Replace the App.css = app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+import "./app.css";
 import { getStudents, deleteStudent } from "./api"; // Added deleteStudent import
 
 import StudentForm from "./components/StudentForm";
