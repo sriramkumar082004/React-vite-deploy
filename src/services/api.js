1bb09aee6d4679cc19c711e72f7355f5a7026494
Sriram Kumar <sriramkumar082004@gmail.com>
2026-01-12 00:37:31 +0530

    Add env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+
+
 const api = axios.create({
-    baseURL: 'https://fast-api-server-bnf5.onrender.com',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
